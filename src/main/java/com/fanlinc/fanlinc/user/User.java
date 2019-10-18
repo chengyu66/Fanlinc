d18bc342dc0e56b67e4cc621d38a825d49295fd0
HomingL <hongming.li@mail.utoronto.ca>
2019-10-18 15:49:35 -0400

    added data dependency, User and its repository

---
+++ src/main/java/com/fanlinc/fanlinc/user/User.java
@@ -0,0 +1,54 @@
+package com.fanlinc.fanlinc.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    private @Id @GeneratedValue Long id;
+    private String firstName;
+    private String lastName;
+    private String description;
+
+    private User() {}
+
+    public User(String firstName, String lastName, String description) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
