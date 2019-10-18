8e167fc38052a8a5af56977e36d74d02723e13e8
Cheng Gao <cheng.gao@ontario.ca>
2019-10-18 15:47:54 -0400

    created Fandom Entity and Fandom Repo

---
+++ src/main/java/com/fanlinc/fanlinc/Fandom.java
@@ -0,0 +1,35 @@
+package com.fanlinc.fanlinc;
+
+import javax.persistence.*;
+
+@Entity
+public class Fandom {
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @Column(name = "fandomId", updatable = false, nullable = false)
+    private Integer fandomId;
+
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @Column(name = "fandomName", nullable = false)
+    private String fandomName;
+
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @Column(name = "fandomOwnerId", updatable = false, nullable = false)
+    private Integer fandomOwnerId;
+
+    public Integer getFandomIdId() {
+        return fandomId;
+    }
+
+    public String getFandomNameName() {
+        return fandomName;
+    }
+
+    public void setFandomName(String fandomName) {
+        this.fandomName = fandomName;
+    }
+
+    public Integer getFandomOwnerId() {
+        return fandomOwnerId;
+    }
+}
