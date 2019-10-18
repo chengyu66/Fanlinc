6e1e19eecda43d4380211aa4dae665f208edb081
HomingL <hongming.li@mail.utoronto.ca>
2019-10-18 14:14:45 -0400

    starter code

---
+++ src/main/java/com/fanlinc/fanlinc/FanlincApplication.java
@@ -0,0 +1,13 @@
+package com.fanlinc.fanlinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanlincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanlincApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanlinc/fanlinc/FanlincApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanlinc.fanlinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanlincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
