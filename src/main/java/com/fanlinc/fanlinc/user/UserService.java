c4c346ba463cea35b9973a1e346ff690431bfe96
liangroy <liangroy@yahoo.com>
2019-10-18 18:27:32 -0400

    Implement UserService and UserController to use api create a new user

--- src/main/java/com/fanlinc/fanlinc/user/UserService.java
+++ src/main/java/com/fanlinc/fanlinc/user/UserService.java
@@ -1,4 +1,20 @@
 package com.fanlinc.fanlinc.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository repo) {
+        this.userRepository = repo;
+    }
+
+    public User save(User user) {
+        userRepository.save(user);
+        return user;
+    }
 }
