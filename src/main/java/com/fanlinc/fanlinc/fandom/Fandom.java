5be9e2e4eb579a240ce7707cf5aa423f4d8b6ae3
chengyu66 <1998xinchengyu@gmail.com>
2019-10-20 23:28:07 -0400

    changed table name

--- src/main/java/com/fanlinc/fanlinc/fandom/Fandom.java
+++ src/main/java/com/fanlinc/fanlinc/fandom/Fandom.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "Fandom")
+@Table(name = "Fandoms")
 public class Fandom {
     @Id
     @GeneratedValue(strategy=GenerationType.SEQUENCE)
--- src/main/java/com/fanlinc/fanlinc/user/User.java
+++ src/main/java/com/fanlinc/fanlinc/user/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity(name = "User") // This tells Hibernate to make a table out of this class
+@Entity(name = "Users") // This tells Hibernate to make a table out of this class
 public class User {
 
     @Id
