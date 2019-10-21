c9c4f3316d96a258ce6c8bd666539f08402280ce
Diego2941 <diego2941@hotmail.com>
2019-10-21 18:26:31 -0400

    Added basic footer, header, navbar and home page

---
+++ frontend/src/components/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom';
+import ApiService from '../../services/apiservice';
+
+class home extends Component{
+    render(){
+        return (
+                    <div>
+                        <h2 className="text-center">Fanlinc</h2>
+                    </div>
+                );
+    }
+}
+export default home;
