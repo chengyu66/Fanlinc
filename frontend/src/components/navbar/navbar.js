f397749484d7baa5ead74e0e536f77113d47c30d
Diego2941 <diego2941@hotmail.com>
2019-10-21 22:04:29 -0400

    Change the link of nav bar

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -8,14 +8,14 @@ class Navbar extends Component{
                     <header>
                           <div class="container">
                             <div id="branding">
-                              <h1><span class="highlight">Fan</span>linx</h1>
+                              <h1><a href="/"><span class="highlight">Fan</span>linx</a></h1>
                             </div>
                             <nav>
                               <ul>
                                 <li><a href="/">Home</a></li>
                                 <li class="current"><a href="#">About</a></li>
-                                <li><a href="#">Services</a></li>
-                                <li><a href="/login">Login</a></li>
+                                <li><a href="#">Fandoms</a></li>
+                                <li><a href="/login">Signin/Signup</a></li>
                               </ul>
                             </nav>
                           </div>
