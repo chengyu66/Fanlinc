433d0080fc3ff89fd8e06bd26bdc93d36021a7e3
chengyu66 <1998xinchengyu@gmail.com>
2019-10-22 11:32:33 -0400

    created exception class for emaill already exists

---
+++ src/main/java/com/fanlinc/fanlinc/EmailExistsException.java
@@ -0,0 +1,13 @@
+package com.fanlinc.fanlinc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Email already registered")
+public class EmailExistsException extends Exception {
+
+    public EmailExistsException(String email) {
+        super("Email already registered : " + email);
+    }
+
+}
