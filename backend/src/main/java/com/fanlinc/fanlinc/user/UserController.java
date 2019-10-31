9bcec73eb6b8163a8cdd7578f7becb6a1bc03815
CakeOrange <gnehc.gao@mail.utoronto.ca>
2019-10-31 19:44:36 -0400

    remove join fandom from user controller

--- backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
+++ backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
@@ -67,19 +67,6 @@ public class UserController {
         return service.findByEmailAndPassword(email, password);
     }
 
-//    @PostMapping(path="/joinFandom") // Map ONLY POST Requests
-//    @ResponseBody
-//    public void JoinFandom (@RequestParam  String email, @RequestParam String fandomName) {
-//        // @ResponseBody means the returned String is the response, not a view name
-//        // @RequestParam means it is a parameter from the GET or POST request
-//        User user = service.findByEmail(email);
-//        Fandom fandom = fservice.findByFandomName(fandomName);
-//        System.out.println(fandom.getFandomId());
-//        System.out.println(user.getId());
-//        fandom.setUsers(user);
-//        user.setFandoms(fandom);
-//        service.save(user);
-//    }
     @CrossOrigin(origins ="*")
     @PostMapping(path="/quitFandom") // Map ONLY POST Requests
     @ResponseBody
