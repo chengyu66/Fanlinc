36096cb2ac7102816d18a15953488294af1f1707
CakeOrange <gnehc.gao@mail.utoronto.ca>
2019-10-31 19:45:06 -0400

    remove quit fandom from user controller

--- backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
+++ backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
@@ -68,24 +68,6 @@ public class UserController {
     }
 
     @CrossOrigin(origins ="*")
-    @PostMapping(path="/quitFandom") // Map ONLY POST Requests
-    @ResponseBody
-    public void QuitFandom (@RequestParam  String email, @RequestParam String fandomName) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-        User user = service.findByEmail(email);
-        Fandom fandom = fservice.findByFandomName(fandomName);
-        System.out.println(fandom.getFandomId());
-        System.out.println(user.getId());
-        System.out.println(fandom.getUser().size());
-        System.out.println(user.getFandoms().size());
-        fandom.getUser().remove(user);
-        user.getFandoms().remove(fandom);
-        System.out.println(fandom.getUser().size());
-        System.out.println(user.getFandoms().size());
-        service.save(user);
-    }
-    @CrossOrigin(origins ="*")
     @GetMapping(path="/findUserByEmail") // Map ONLY GET Requests
     @ResponseBody
     public User findUserByEmail (@RequestParam String email) {
