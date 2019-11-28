2d9c6256b6d76af1a333a9ecf66ec300ed7af18a
CakeOrange <gnehc.gao@mail.utoronto.ca>
2019-11-28 18:47:05 -0500

    implement file uploading/downloading feature

--- backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
+++ backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
@@ -10,9 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -51,20 +56,31 @@ public class UserController {
     }
 
     @GetMapping("/downloadFile")
-    public Resource downloadFile(@RequestParam("email") String email){
+    public ResponseEntity<Resource> downloadFile(@RequestParam("email") String email,
+                                                 HttpServletRequest request){
         User user = service.findByEmail(email);
         String fileName = user.getProfile_pic();
+        // Load file as Resource
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
         try {
-            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
-            if(resource.exists()) {
-                return resource;
-            } else {
-                throw new MyFileNotFoundException("File not found " + fileName);
-            }
-        } catch (MalformedURLException ex) {
-            throw new MyFileNotFoundException("File not found " + fileName, ex);
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
         }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return  ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+
     }
 
     @CrossOrigin(origins = "*")
