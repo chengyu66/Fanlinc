669b095fdaf391e4228b689e65ab46bb221d18b5
CakeOrange <gnehc.gao@mail.utoronto.ca>
2019-11-28 16:47:58 -0500

    implement file uploading/downloading feature

--- backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
+++ backend/src/main/java/com/fanlinc/fanlinc/user/UserController.java
@@ -1,25 +1,43 @@
 package com.fanlinc.fanlinc.user;
 
 import com.fanlinc.fanlinc.exceptions.EmailExistsException;
-import com.fanlinc.fanlinc.fandom.Fandom;
 import com.fanlinc.fanlinc.fandom.FandomService;
-//import com.fanlinc.fanlinc.fandom.FandomId;
+import com.fanlinc.fanlinc.payload.UploadFileResponse;
+import com.fanlinc.fanlinc.service.FileStorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
-//import com.fanlinc.fanlinc.fandom.FandomId;
 
 @RestController    // This means that this class is a Controller
 @RequestMapping(path = "/api/users") // This means URL's start with /demo (after Application path)
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserService service;
     private final FandomService fservice;
+    @Autowired
+    private FileStorageService fileStorageService;
 
     public UserController(UserService service, FandomService fservice) {
         this.service = service;
         this.fservice = fservice;
+    }
+
+
+    @PostMapping("/uploadFile")
+    public User uploadFile(@RequestParam("file") MultipartFile file,
+                                         @RequestParam("email") String email) {
+        String fileName = fileStorageService.storeFile(file);
+        User user = service.findByEmail(email);
+        Long uid = user.getId();
+        user.setProfile_pic(uid.toString()+fileName);
+        return service.save(user);
     }
 
     @CrossOrigin(origins = "*")
@@ -35,6 +53,8 @@ public class UserController {
         } 
         return service.save(newUser);
     }
+
+
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/updateUser") // Map ONLY POST Requests
