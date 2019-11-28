0d1278de711b0573f1adda71500cab16468b807f
CakeOrange <gnehc.gao@mail.utoronto.ca>
2019-11-28 17:45:06 -0500

    implement file uploading/downloading feature

--- backend/src/main/java/com/fanlinc/fanlinc/FanlincApplication.java
+++ backend/src/main/java/com/fanlinc/fanlinc/FanlincApplication.java
@@ -1,11 +1,15 @@
 package com.fanlinc.fanlinc;
 
 
+import com.fanlinc.fanlinc.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class FanlincApplication {
 
 	public static void main(String[] args) {
