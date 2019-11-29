4aa9757a054d6bbf23ef587aac83f8df741ea7e9
CakeOrange <gnehc.gao@mail.utoronto.ca>
2019-11-29 14:56:52 -0500

    implement file uploading/downloading feature for post

--- backend/src/main/java/com/fanlinc/fanlinc/post/PostController.java
+++ backend/src/main/java/com/fanlinc/fanlinc/post/PostController.java
@@ -1,9 +1,16 @@
 package com.fanlinc.fanlinc.post;
 
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
+import com.fanlinc.fanlinc.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +18,7 @@ import com.fanlinc.fanlinc.fandom.Fandom;
 import com.fanlinc.fanlinc.fandom.FandomService;
 import com.fanlinc.fanlinc.user.User;
 import com.fanlinc.fanlinc.user.UserService;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController    // This means that this class is a Controller
 @RequestMapping(path = "/api/posts") // This means URL's start with /demo (after Application path)
@@ -18,13 +26,40 @@ public class PostController {
 	private final UserService service;
     private final FandomService fservice;
     private final PostService pservice;
+    @Autowired
+    private FileStorageService fileStorageService;
 
     public PostController(UserService service, FandomService fservice, PostService pservice) {
         this.service = service;
         this.fservice = fservice;
         this.pservice = pservice;
     }
-    
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/uploadFile")
+    public Post uploadFile(@RequestParam("file") MultipartFile file,
+                           @RequestParam("pid") Long pid) {
+        Post post = pservice.findByPostId(pid);
+        String fileName = "id" + pid.toString() + "_post_picture";
+        fileStorageService.storeFile(file,fileName);
+        post.setPostPic(fileName);
+        return pservice.save(post);
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/downloadFile")
+    public ResponseEntity<ByteArrayResource> downloadFile(@RequestParam("pid") Long pid){
+        Post post = pservice.findByPostId(pid);
+        String fileName = post.getPostPic();
+        System.out.println(fileName);
+        byte[] data = Base64.getEncoder().encodeToString(fileStorageService.downloadFile(fileName)).getBytes();
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + fileName + "\"")
+                .contentLength(data.length)
+                .body(new ByteArrayResource(data));
+    }
+
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/post") // Map ONLY POST Requests
     public Post post (@RequestBody Post post) {
